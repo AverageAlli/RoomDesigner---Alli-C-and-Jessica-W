8e52ccad0e3a21454dc69059d97e36e4e84bd6c0
AverageAlli <allison.carr@bsu.edu>
2024-03-12 11:45:26 -0400

    Create furniture test class that creates furniture and tests the setters and getters of the furniture class.

--- src/main/java/edu/bsu/cs/Furniture.java
+++ src/main/java/edu/bsu/cs/Furniture.java
@@ -1,4 +1,32 @@
 package edu.bsu.cs;
 
 public class Furniture {
+    private String name;
+    private double width;
+    private double length;
+
+    public Furniture(String name, double width, double length) {
+        this.name = name;
+        this.width = width;
+        this.length = length;
+    }
+    //Getters & Setters
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public double getWidth() {
+        return width;
+    }
+    public void setWidth(double width) {
+        this.width = width;
+    }
+    public double getLength() {
+        return length;
+    }
+    public void setLength(double length) {
+        this.length = length;
+    }
 }
--- src/test/java/edu/bsu/cs/RoomTest.java
+++ src/test/java/edu/bsu/cs/RoomTest.java
@@ -1,2 +1,4 @@
+package edu.bsu.cs;
+
 public class RoomTest {
 }
