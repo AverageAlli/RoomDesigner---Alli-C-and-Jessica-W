7c7ffa839ca1398df2bcf7591920058ca2ce8e3a
jwalter03 <jlwalter03@gmail.com>
2024-03-12 10:19:11 -0400

    Started adding code for the RoomMaker class

---
+++ src/main/java/edu/bsu/cs/RoomMaker.java
@@ -0,0 +1,14 @@
+package edu.bsu.cs;
+
+import java.util.Scanner;
+
+public class RoomMaker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter height of room:");
+        int height = scanner.nextInt();
+        System.out.println("Enter Width of room:");
+        int width = scanner.nextInt();
+
+    }
+}
