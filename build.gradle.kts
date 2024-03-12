b28bab07c2f9ec414158a139b2e09e0fb3e007b9
AverageAlli <allison.carr@bsu.edu>
2024-03-12 12:54:52 -0400

    Add new dependencies to build.gradle.kts file

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,17 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    // Apiguardian API
+    implementation("org.apiguardian:apiguardian-api:1.1.2")
+
+    // JUnit 5
+    implementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("org.junit.jupiter:junit-jupiter-params")
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.platform:junit-platform-commons")
+    testImplementation("org.opentest4j:opentest4j:1.2.0")
 }
 
 tasks.test {
