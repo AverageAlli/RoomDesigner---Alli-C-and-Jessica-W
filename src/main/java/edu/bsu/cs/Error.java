ef40e774fb4177624e600bdeaf17530095230589
AverageAlli <allison.carr@bsu.edu>
2024-03-12 11:21:50 -0400

    Add alert for javaFX for when 'Validation' class is deemed 'violated'

--- src/main/java/edu/bsu/cs/Error.java
+++ src/main/java/edu/bsu/cs/Error.java
@@ -1,4 +1,15 @@
 package edu.bsu.cs;
 
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
 public class Error {
+    public static void showError(String message) {
+        Alert alert = new Alert(AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
 }
+//use by calling Error.showError("MESSAGE")
