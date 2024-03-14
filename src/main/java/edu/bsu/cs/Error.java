d4bef0c9cd4dea022deb6c7da184047c52afea5b
jwalter03 <jlwalter03@gmail.com>
2024-03-14 10:01:30 -0400

    implemented javafx

--- src/main/java/edu/bsu/cs/Error.java
+++ src/main/java/edu/bsu/cs/Error.java
@@ -1,15 +1,17 @@
 package edu.bsu.cs;
 
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
+//import javafx.scene.control.Alert;
+//import javafx.scene.control.Alert.AlertType;
 
 public class Error {
-    public static void showError(String message) {
+ /*   public static void showError(String message) {
         Alert alert = new Alert(AlertType.ERROR);
         alert.setTitle("Error");
         alert.setHeaderText(null);
         alert.setContentText(message);
         alert.showAndWait();
     }
-}
+
 //use by calling Error.showError("MESSAGE")
+*/
+}
--- src/main/java/edu/bsu/cs/Validation.java
+++ src/main/java/edu/bsu/cs/Validation.java
@@ -1,12 +1,12 @@
 package edu.bsu.cs;
 
 public class Validation {
-    public static boolean isValidRoomDimensions(double length, double width) {
+   /* public static boolean isValidRoomDimensions(double length, double width) {
         if (length <= 0 || width <= 0) {
             Error.showError("Invalid room dimensions. Length and width must be positive.");
             return false;
         }
         return true;
-    }
+    }*/
 }
 //Still need to add a maximum for room size, need to do testing on personal computers to determine what that is
