0bd69e8f9dcde80da4ec7b498e0338f87e9fd510
jwalter03 <jlwalter03@gmail.com>
2024-03-18 11:42:14 -0400

    Updated gradle

--- src/test/java/edu/bsu/cs/ErrorTest.java
+++ src/test/java/edu/bsu/cs/ErrorTest.java
@@ -2,6 +2,7 @@ package edu.bsu.cs;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
 public class ErrorTest extends Application {
@@ -12,7 +13,7 @@ public class ErrorTest extends Application {
     }
 
     @Override
-    public void start(javafx.stage.Stage primaryStage) {
+    public void start(Stage primaryStage) {
         // This method is called when the JavaFX application is launched
         // You can leave this method empty
     }
@@ -20,18 +21,47 @@ public class ErrorTest extends Application {
     @Override
     public void stop() {
         // Stop the JavaFX application when the test is finished
-        Platform.exit();
+        System.exit(0);
     }
-    @Override
-    public void init() throws Exception {
-        super.init();
-        // Add any cleanup code here
-    }
+
     public void testError() {
         // Test showing an error message
-        Error.showError("This is an error message.");
+        showError("This is an error message.");
 
         // Test showing another error message
-        Error.showError("Another error occurred.");
+        showError("Another error occurred.");
     }
+
+    public static void showError(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+//    @Override
+//    public void start(javafx.stage.Stage primaryStage) {
+//        // This method is called when the JavaFX application is launched
+//        // You can leave this method empty
+//    }
+//
+//    @Override
+//    public void stop() {
+//        // Stop the JavaFX application when the test is finished
+//        Platform.exit();
+//    }
+//    @Override
+//    public void init() throws Exception {
+//        super.init();
+//        // Add any cleanup code here
+//    }
+//    public void testError() {
+//        // Test showing an error message
+//        Error.showError("This is an error message.");
+//
+//        // Test showing another error message
+//        Error.showError("Another error occurred.");
+//    }
+
 }
--- src/test/java/edu/bsu/cs/RoomTest.java
+++ src/test/java/edu/bsu/cs/RoomTest.java
@@ -1,25 +1,76 @@
 package edu.bsu.cs;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RoomTest {
+    private RoomMaker roomMaker;
+    private Furniture chair;
+    private Furniture table;
+
+//    @BeforeEach
+//    public void setUp() {
+//        roomMaker = new RoomMaker(10.5, 8.3);
+//        chair = new Furniture("Chair");
+//        table = new Furniture("Table");
+//    }
+
     @Test
-    public void testValidRoomMakerToString(){
-        //Arrange
-        int length = 5;
-        int width = 5;
-        RoomMaker roomMaker = new RoomMaker(length, width);
-        String expected = "Room: Length=5, Width=5";
-
-        // Act
-        String actual = roomMaker.toString();
-
-        // Assert
-        assertEquals(expected, actual);
+    public void testAddFurniture() {
+        roomMaker.addFurniture(chair);
+        roomMaker.addFurniture(table);
+        List<Furniture> expectedFurnitureList = new ArrayList<>();
+        expectedFurnitureList.add(chair);
+        expectedFurnitureList.add(table);
+        assertEquals(expectedFurnitureList, roomMaker.getFurnitureList());
     }
+
+    @Test
+    public void testToString() {
+        roomMaker.addFurniture(chair);
+        roomMaker.addFurniture(table);
+        String expectedString = "Room Dimensions: 10x8\n" +
+                "Furniture:\n" +
+                "- Chair\n" +
+                "- Table\n";
+        assertEquals(expectedString, roomMaker.toString());
+    }
+
+    @Test
+    public void testGetLength() {
+        assertEquals(10, roomMaker.getLength());
+    }
+
+    @Test
+    public void testGetWidth() {
+        assertEquals(8, roomMaker.getWidth());
+    }
+
+    @Test
+    public void testSetLength() {
+        roomMaker.setLength(12);
+        assertEquals(12, roomMaker.getLength());
+    }
+
+    @Test
+    public void testSetWidth() {
+        roomMaker.setWidth(9);
+        assertEquals(9, roomMaker.getWidth());
+    }
+
+    @Test
+    public void testSetFurnitureList() {
+        List<Furniture> newFurnitureList = new ArrayList<>();
+        newFurnitureList.add(chair);
+        roomMaker.setFurnitureList(newFurnitureList);
+        assertEquals(newFurnitureList, roomMaker.getFurnitureList());
+    }
+
     }
 
