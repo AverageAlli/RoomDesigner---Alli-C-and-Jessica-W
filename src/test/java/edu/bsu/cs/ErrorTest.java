515f6b70c9e19bb95a7bca1a6d1bbc6f28ee4250
AverageAlli <allison.carr@bsu.edu>
2024-03-18 00:03:24 -0400

    Create errorTest and validationTest as well as removing unnecessary GUI class

--- src/test/java/edu/bsu/cs/ErrorTest.java
+++ src/test/java/edu/bsu/cs/ErrorTest.java
@@ -1,4 +1,37 @@
 package edu.bsu.cs;
 
-public class ErrorTest {
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.stage.Stage;
+
+public class ErrorTest extends Application {
+
+    public static void main(String[] args) {
+        // Launch a simple JavaFX application to initialize the toolkit
+        launch(args);
+    }
+
+    @Override
+    public void start(javafx.stage.Stage primaryStage) {
+        // This method is called when the JavaFX application is launched
+        // You can leave this method empty
+    }
+
+    @Override
+    public void stop() {
+        // Stop the JavaFX application when the test is finished
+        Platform.exit();
+    }
+    @Override
+    public void init() throws Exception {
+        super.init();
+        // Add any cleanup code here
+    }
+    public void testError() {
+        // Test showing an error message
+        Error.showError("This is an error message.");
+
+        // Test showing another error message
+        Error.showError("Another error occurred.");
+    }
 }
--- src/test/java/edu/bsu/cs/ValidationTest.java
+++ src/test/java/edu/bsu/cs/ValidationTest.java
@@ -1,4 +1,31 @@
 package edu.bsu.cs;
 
+import javax.swing.*;
+
 public class ValidationTest {
+
+    public static void main(String[] args) {
+        ValidationTest validationTest = new ValidationTest();
+        validationTest.testValidation();
+    }
+
+    public void testValidation() {
+        // Test valid room dimensions
+        double validLength = 10.0;
+        double validWidth = 8.0;
+        boolean isValid = Validation.isValidRoomDimensions(validLength, validWidth);
+        System.out.println("Are dimensions valid? " + isValid);
+
+        // Test invalid room dimensions (negative length)
+        double invalidLength = -5.0;
+        double width = 8.0;
+        isValid = Validation.isValidRoomDimensions(invalidLength, width);
+        System.out.println("Are dimensions valid? " + isValid);
+
+        // Test invalid room dimensions (zero width)
+        double length = 10.0;
+        double invalidWidth = 0.0;
+        isValid = Validation.isValidRoomDimensions(length, invalidWidth);
+        System.out.println("Are dimensions valid? " + isValid);
+    }
 }
