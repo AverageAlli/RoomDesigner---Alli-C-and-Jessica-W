72fc8fad0322b3aa74d11117062dea401b776122
AverageAlli <allison.carr@bsu.edu>
2024-03-18 00:34:46 -0400

    Create errorTest and validationTest as well as removing unnecessary GUI class

---
+++ src/main/java/edu/bsu/cs/Main.java
@@ -0,0 +1,35 @@
+package edu.bsu.cs;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Load FXML file
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("GUI.fxml"));
+        Parent root = loader.load();
+
+        // Set controller
+        RoomDesignerController controller = loader.getController();
+        // You may need to pass any necessary data to the controller
+        // For example:
+        // controller.initData();
+
+        // Create scene
+        Scene scene = new Scene(root);
+
+        // Set scene and show stage
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Room Designer");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
