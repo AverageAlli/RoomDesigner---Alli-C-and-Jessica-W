e9a4edfe46db38fcdc79b8fc4d53b550c71d9a57
AverageAlli <allison.carr@bsu.edu>
2024-03-20 00:17:23 -0400

    Make CLI and GUI accept the new PNG's as an input

--- src/main/java/edu/bsu/cs/BedroomDesignerCLI.java
+++ src/main/java/edu/bsu/cs/BedroomDesignerCLI.java
@@ -1,6 +1,50 @@
 package edu.bsu.cs;
 
-import javax.swing.*;
+import java.util.Scanner;
+
+public class BedroomDesignerCLI {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Bedroom Designer by Alli and Jess!");
+        System.out.println("Please select one of the 6 pre-selected floor plans below or type 'e' to exit:");
+        System.out.println("1. 9x16");
+        System.out.println("2. 10x8");
+        System.out.println("3. 10x12");
+        System.out.println("4. 10x14");
+        System.out.println("5. 12x8");
+        System.out.println("6. 12x12");
+
+        while (true) {
+            System.out.print("Enter your choice (1-6) or 'e' to exit: ");
+            String choice = scanner.nextLine();
+
+            if (choice.equalsIgnoreCase("e")) {
+                System.out.println("Goodbye!");
+                break;
+            }
+
+            int roomSize;
+            try {
+                roomSize = Integer.parseInt(choice);
+                if (roomSize < 1 || roomSize > 6) {
+                    throw new NumberFormatException();
+                }
+            } catch (NumberFormatException e) {
+                System.err.println("Invalid input. Please enter a number between 1 and 6 or 'e' to exit.");
+                continue;
+            }
+
+            openBedroomDesignerGUI(roomSize);
+        }
+    }
+
+    private static void openBedroomDesignerGUI(int roomSize) {
+        System.out.println("You chose layout option " + roomSize + ". Generating GUI...");
+        new BedroomDesignerGUI(roomSize);
+    }
+}
+/*import javax.swing.*;
 import java.util.Scanner;
 
 public class BedroomDesignerCLI {
@@ -75,4 +119,4 @@ public class BedroomDesignerCLI {
         });
 
         System.out.println("Thank you for using Bedroom Designer. Goodbye!");
-    }}
+    }}*/
--- src/main/java/edu/bsu/cs/BedroomDesignerGUI.java
+++ src/main/java/edu/bsu/cs/BedroomDesignerGUI.java
@@ -2,6 +2,79 @@ package edu.bsu.cs;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+public class BedroomDesignerGUI extends JFrame {
+
+    private static final String[] FURNITURE_NAMES = {"Bed", "Nightstand", "Dresser", "Chair"};
+    private static final String[] FURNITURE_IMAGE_NAMES = {"bed.png", "nightstand.png", "dresser.png", "chair.png"};
+    private static final String ROOM_LAYOUTS_DIRECTORY = "/Users/allicarr/IdeaProjects/FinalProject-JessicaAlli/src/main/roomLayouts/";
+
+    private JPanel roomPanel;
+    private JComboBox<String> furnitureComboBox;
+
+    public BedroomDesignerGUI(int roomSize) {
+        setTitle("Bedroom Designer");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(900, 900);
+        setLayout(new BorderLayout());
+
+        // Load background image based on room size
+        String roomImagePath = ROOM_LAYOUTS_DIRECTORY + "room" + roomSize + ".png";
+        ImageIcon backgroundImageIcon = new ImageIcon(roomImagePath);
+        JLabel backgroundLabel = new JLabel(backgroundImageIcon);
+        roomPanel = new JPanel(new BorderLayout());
+        roomPanel.add(backgroundLabel);
+
+        // Add furniture combo box
+        furnitureComboBox = new JComboBox<>(FURNITURE_NAMES);
+        furnitureComboBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String selectedFurniture = (String) furnitureComboBox.getSelectedItem();
+                String furnitureImageName = getFurnitureImageName(selectedFurniture);
+                String furnitureImagePath = ROOM_LAYOUTS_DIRECTORY + furnitureImageName;
+                ImageIcon furnitureImageIcon = new ImageIcon(furnitureImagePath);
+                JLabel furnitureLabel = new JLabel(furnitureImageIcon);
+                furnitureLabel.setSize(furnitureImageIcon.getIconWidth(), furnitureImageIcon.getIconHeight());
+                furnitureLabel.setLocation(0, 0);
+                roomPanel.add(furnitureLabel);
+                roomPanel.revalidate();
+                roomPanel.repaint();
+            }
+        });
+
+        JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        topPanel.add(furnitureComboBox);
+
+        add(topPanel, BorderLayout.NORTH);
+        add(roomPanel, BorderLayout.CENTER);
+
+        setVisible(true);
+    }
+
+    private String getFurnitureImageName(String furnitureName) {
+        for (int i = 0; i < FURNITURE_NAMES.length; i++) {
+            if (FURNITURE_NAMES[i].equals(furnitureName)) {
+                return FURNITURE_IMAGE_NAMES[i];
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new BedroomDesignerGUI(1); // Default room size
+            }
+        });
+    }
+}
+/*import javax.swing.*;
+import java.awt.*;
 
 
 public class BedroomDesignerGUI extends JFrame {
@@ -73,4 +146,4 @@ public class BedroomDesignerGUI extends JFrame {
             System.out.println("Please provide the room dimensions (length and width) as command line arguments.");
         }
     }
-}
+}*/
