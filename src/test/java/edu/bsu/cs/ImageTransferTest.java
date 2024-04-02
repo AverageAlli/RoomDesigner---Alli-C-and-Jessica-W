151fd2593c1bf37175ceef982a6fd8d6f93bc6cb
AverageAlli <allison.carr@bsu.edu>
2024-04-02 10:38:18 -0400

    Make ErrorTest, FurnitureTest, ImageTransferTest, RoomMakerTest, and ValidationTest all workable and passing.

--- src/test/java/edu/bsu/cs/ImageTransferTest.java
+++ src/test/java/edu/bsu/cs/ImageTransferTest.java
@@ -1,4 +1,47 @@
+
 package edu.bsu.cs;
 
+import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
 public class ImageTransferTest {
-}
+    @Test
+    public void testTransferDataFlavors() {
+        Image image = new ImageIcon("src/main/ObjectImages/Bed.png").getImage();
+        ImageTransfer imageTransfer = new ImageTransfer(image);
+        DataFlavor[] flavors = imageTransfer.getTransferDataFlavors();
+
+        // Verify that the only supported flavor is DataFlavor.imageFlavor
+        assertEquals(1, flavors.length);
+        assertTrue(flavors[0].equals(DataFlavor.imageFlavor));
+        }
+
+        @Test
+        public void testIsDataFlavorSupported() {
+        Image image = new ImageIcon("src/main/ObjectImages/Bed.png").getImage();
+        ImageTransfer imageTransfer = new ImageTransfer(image);
+
+        // Verify that DataFlavor.imageFlavor is supported
+        assertTrue(imageTransfer.isDataFlavorSupported(DataFlavor.imageFlavor));
+        }
+
+        @Test
+        public void testGetTransferData() throws UnsupportedFlavorException, IOException {
+        Image image = new ImageIcon("src/main/ObjectImages/Bed.png").getImage();
+        ImageTransfer imageTransfer = new ImageTransfer(image);
+
+        // Verify that getTransferData returns the expected Image
+        assertEquals(image, imageTransfer.getTransferData(DataFlavor.imageFlavor));
+        }
+        }
